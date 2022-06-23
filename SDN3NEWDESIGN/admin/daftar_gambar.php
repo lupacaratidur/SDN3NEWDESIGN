4f3bc6a6e8f335df5e89f1ba69aefe10dfbacfbe
lupacaratidur <king.wahrul@gmail.com>
2022-06-23 16:38:27 +0700

    progres

--- SDN3NEWDESIGN/admin/daftar_gambar.php
+++ SDN3NEWDESIGN/admin/daftar_gambar.php
@@ -10,18 +10,3 @@ foreach ($files as $file) {
 header('Content-Type: application/json');
 echo json_encode($response);
 die();
-
-<?php
-$files = array_filter(glob('../assets/img/upload_an/*'), 'is_file');
-
-$response = [];
-
-foreach ($files as $file) {
-	$response[] = basename($file);
-}
-
-header('Content-Type: application/json');
-echo json_encode($response);
-die();
-
-?>
